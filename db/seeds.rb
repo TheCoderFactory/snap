7abddad5db0b80c929f924082f74cec015af52dc
ksilitonga@hotmail.com <ksilitonga@hotmail.com>
2015-06-03 13:19:47 +1000

    Added the new column views in photos table and added increment! method in controller, and added the views number in the html file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+album1 = Album.create!(name: "natures")
+album2 = Album.create!(name: "cars")
+album3 = Album.create!(name: "flowers")
+album4 = Album.create!(name: "sunsets")
+
+photo1 = Photo.create!(album_id: 1, image: "http://linzhouweb.com/wp-content/uploads/2015/118651-natures.jpeg", description: "A foothpath in the middle of the fern forest")
+photo2 = Photo.create!(album_id: 2, image: "http://media.caranddriver.com/images/media/51/2014-10best-cars-header-photo-553289-s-original.jpg", description: "A red sporty car")
