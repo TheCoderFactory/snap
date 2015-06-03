8a95f590d10e62f0af70c68f8980b18ec0bc00cc
Bryan Summers <bsummersgb@gmail.com>
2015-06-03 15:15:12 +1000

    Add all_photos controller and corresponding index view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :albums do
     resources :photos
   end
+
+  get 'all_photos' => 'all_photos#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
